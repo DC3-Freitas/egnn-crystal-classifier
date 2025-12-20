83efbcd0857f7486563267f71d18f01571400669
Alexander Liang <alexlliang0@gmail.com>
2025-12-20 12:21:45 -0800

    chore: fix docstring

--- src/egnn_crystal_classifier/nequip/other_structures/outlier_data.py
+++ src/egnn_crystal_classifier/nequip/other_structures/outlier_data.py
@@ -14,10 +14,10 @@ class OutlierData:
     Information used for unknown structure classification.
 
     Attributes:
-        perfect_embeddings (num_crystals, sz): ideal embeddings for each crystal type
+        perfect_embeddings (num_crystals, sz): Ideal embeddings for each crystal type
 
-        (num_crystals, sz) numpy array of ideal embeddings for each crystal
-        type with index determined on label type.
+        delta_cutoffs (num_crystals,): Distance cutoffs for each crystal type (i.e. if distance
+                                       is >= cutoff then it is unknown).
     """
 
     perfect_embeddings: npt.NDArray[np.float32]
